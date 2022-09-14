c3d93b6e266be107c2943ed5e7937926a13fa1ff
Dhaneshi2i <112234471+Dhaneshi2i@users.noreply.github.com>
2022-09-14 12:06:13 +0530

    Employee

---
+++ logger3@/com/ideas2it/employee/dto/TrainerDto.java
@@ -0,0 +1,43 @@
+package com.ideas2it.employee.dto;
+
+import java.util.List;
+
+public class TrainerDto extends EmployeeDto {
+
+    private int trainerId;
+    private byte projectsWorked;
+    private List<TraineeDto> trainees;
+
+    public void setTrainerId(int trainerId) {
+        this.trainerId = trainerId;
+    }
+
+    public int getTrainerId() {
+        return trainerId;
+    }
+
+    public void setProjectsWorked(byte projectsWorked) {
+	this.projectsWorked = projectsWorked;
+    }
+
+    public byte getProjectsWorked() {
+	return projectsWorked;
+    } 
+
+    public void setTrainee(List<TraineeDto> trainees) {
+        this.trainees = trainees;
+    }
+
+    public List<TraineeDto> getTrainee() {
+        return trainees;
+    }
+
+    public String toString() {
+	StringBuilder stringBuilder = new StringBuilder();
+	stringBuilder.append(super.toString())
+                     .append("\nProjects worked: ")
+		     .append(this.getProjectsWorked());
+
+	return stringBuilder.toString();
+    }
+}
---
+++ logger3@/com/ideas2it/employee/entity/Trainer.java
@@ -0,0 +1,53 @@
+package com.ideas2it.employee.entity;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQuery;
+import javax.persistence.NamedQueries;
+import javax.persistence.Table;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "get_all_trainers",
+                query = "from Trainer where isActiveEmployee = :isActiveEmployee"),
+    @NamedQuery(name = "get_trainer_by_id",
+                query = "from Trainer where isActiveEmployee = :isActiveEmployee AND id = :id") })
+public class Trainer extends Employee {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private int trainerId;
+
+    private byte projectsWorked;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER,mappedBy = "trainers")
+    private List<Trainee> trainees;
+
+    public void setTrainee(List<Trainee> trainees) {
+        this.trainees = trainees;
+    }
+
+    public List<Trainee> getTrainee() {
+        return trainees;
+    }
+
+    public void setProjectsWorked(byte projectsWorked) {
+	this.projectsWorked = projectsWorked;
+    }
+
+    public byte getProjectsWorked() {
+	return projectsWorked;
+    } 
+
+}
